8939ec50a5242599b59a4f1af8659cad535b01ed
Vinay-dev1345 <108272263+Vinay-dev1345@users.noreply.github.com>
2022-07-29 00:22:32 +0530

    initalizied Repository

---
+++ src/main/java/com/example/microservice/packagingAndDelivery/PackagingAndDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservice.packagingAndDelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackagingAndDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackagingAndDeliveryApplication.class, args);
+	}
+
+}
