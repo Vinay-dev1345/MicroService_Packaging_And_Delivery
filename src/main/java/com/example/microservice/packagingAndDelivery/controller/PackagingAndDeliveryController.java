5d27e4306c8483e90ca1a416f1b2cb22798343cb
Vinay-dev1345 <108272263+Vinay-dev1345@users.noreply.github.com>
2022-08-02 01:47:27 +0530

    Added cost compute functionality based on params received

--- src/main/java/com/example/microservice/packagingAndDelivery/controller/PackagingAndDeliveryController.java
+++ src/main/java/com/example/microservice/packagingAndDelivery/controller/PackagingAndDeliveryController.java
@@ -1,16 +1,37 @@
 package com.example.microservice.packagingAndDelivery.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.microservice.packagingAndDelivery.service.PackagingAndDeliveryService;
 
 @RestController
 @RequestMapping("v1/packagingAndDelivery")
 public class PackagingAndDeliveryController {
 	
-	@GetMapping("/costs")
-	public ResponseEntity<?> getCostingData(){
-		return null;
+	@Autowired
+	PackagingAndDeliveryService packagingAndDeliveryService;
+	
+	@GetMapping("/getcost")
+	public ResponseEntity<?> getCostingData(@RequestParam("type") String itemType , @RequestParam("count") int qty){
+		Map<String , Object> responseBody = new HashMap<String , Object>();
+		if(qty > 0) {
+			double response =  packagingAndDeliveryService.computePackagingAndDeliveryCost(itemType, qty);
+			responseBody.put("packagingAndDeliveryCost", response);
+			responseBody.put("errors", false);
+			return ResponseEntity.ok(responseBody);
+		}else {
+			responseBody.put("errors", true);
+			return ResponseEntity.status(HttpStatus.CONFLICT).body(responseBody);
+		}
+		
 	}
 }
--- src/main/java/com/example/microservice/packagingAndDelivery/service/PackagingAndDeliveryService.java
+++ src/main/java/com/example/microservice/packagingAndDelivery/service/PackagingAndDeliveryService.java
@@ -1,8 +1,61 @@
 package com.example.microservice.packagingAndDelivery.service;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.microservice.packagingAndDelivery.Repository.PackagingAndDeliveryRepository;
+import com.example.microservice.packagingAndDelivery.entity.PackagingAndDeliveryCosting;
 
 @Service
 public class PackagingAndDeliveryService {
+	
+	public static final String IIP = "integralItemPackaging";
+	public static final String AIP = "accessoryItemPackaging";
+	public static final String PS = "protectiveSheath";
+	public static final String IID = "integralItemDelivery";
+	public static final String AID = "accessoryItemDelivery";
+	
+	@Autowired
+	PackagingAndDeliveryRepository packagingAndDeliveryRepository;
+	
+	public Map<String , String> loadPriceData(){
+		List<PackagingAndDeliveryCosting> costData = packagingAndDeliveryRepository.findAll();
+		Map<String , String > mappedCostData = new HashMap<String , String>();
+		
+		for(int index = 0 ; index<costData.size() ; index++) {
+			PackagingAndDeliveryCosting pd = costData.get(index);
+			mappedCostData.put(pd.getCostType(), pd.getPrice());
+		}
+		System.out.println(mappedCostData);
+		return mappedCostData;
+	}
+	
+	public double computePackagingAndDeliveryCost(String type , int count) {
+		double finalCost = -1;
+		Map<String, String>loadCostData = loadPriceData();
+		
+		try{
+			if(type.equalsIgnoreCase("Integral")) {
+				double pCost = Double.parseDouble(loadCostData.get(IIP)) + Double.parseDouble(loadCostData.get(PS));
+				double dCost = Double.parseDouble(loadCostData.get(IID));
+				
+				finalCost = (pCost + dCost) * count;
+				}
 
+			if(type.equalsIgnoreCase("Accessory")) {
+				double pCost = Double.parseDouble(loadCostData.get(AIP)) + Double.parseDouble(loadCostData.get(PS));
+				double dCost = Double.parseDouble(loadCostData.get(AID));
+				
+				finalCost = (pCost + dCost) * count;
+				}
+			
+			}catch(Exception e) {
+				System.out.println(e.toString());
+			}
+		return finalCost;
+	}
 }
