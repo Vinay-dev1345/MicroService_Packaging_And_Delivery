97bc5f48467fe0e64fc78d195d4eb3c90377fb0a
Vinay-dev1345 <108272263+Vinay-dev1345@users.noreply.github.com>
2022-08-18 22:10:52 +0530

    Added prod config for h2 and response return type

--- src/main/java/com/example/microservice/packagingAndDelivery/controller/PackagingAndDeliveryController.java
+++ src/main/java/com/example/microservice/packagingAndDelivery/controller/PackagingAndDeliveryController.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,7 +26,7 @@ public class PackagingAndDeliveryController {
 	@Autowired
 	PackagingAndDeliveryService packagingAndDeliveryService;
 	
-	@GetMapping("/getcost")
+	@GetMapping(value="/getcost" , produces= {MediaType.APPLICATION_JSON_VALUE})
 	public ResponseEntity<?> getCostingData(@RequestParam("type") String itemType , @RequestParam("count") int qty){
 		Map<String , Object> responseBody = new HashMap<String , Object>();
 		try {
